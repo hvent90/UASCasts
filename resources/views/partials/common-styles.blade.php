4349585770658c6aa2ab07316c7c8f839c2c70ed
Henry Ventura <hvent90@gmail.com>
2015-04-07 13:51:20 -0400

    added favi

--- resources/views/partials/common-styles.blade.php
+++ resources/views/partials/common-styles.blade.php
@@ -10,7 +10,7 @@
 
 </script>
 
-<link rel="shortcut icon" href="{{ asset('img/favicon.ico') }}">
+<link rel="shortcut icon" href="{{ asset('system/favicon.ico') }}">
 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 <!-- Fonts -->
 <link href='http://fonts.googleapis.com/css?family=Roboto:500,900,400italic,100,300,700,400' rel='stylesheet' type='text/css'>
